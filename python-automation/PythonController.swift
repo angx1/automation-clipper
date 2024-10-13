50c93694683ede31051c05f5415faa4c03776c71
angel <angel.rodriguez3rm@gmail.com>
2024-10-13 19:37:04 +0200

    Update README.md

--- python-automation/PythonController.swift
+++ python-automation/PythonController.swift
@@ -11,7 +11,7 @@ import PythonKit
 class PythonController {
     func runSortDownloads(script: String) -> PythonObject {
         let sys = Python.import("sys")
-        sys.path.append("{path}/python-automation/python-automation/Scripts") // Complete with your global path to ./Scripts
+        sys.path.append("{/Users/...}/python-automation/python-automation/Scripts") // Complete with your global path to ./Scripts
         let file = Python.import("AutomationScript")
         let response = file.sort_downloads(script)
 
